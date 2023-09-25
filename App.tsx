a67688ef9c34aa9b5e63e968345910a422fd52a0
Hemanth-Kumar-04 <hemanthkumar.k.r04@gmail.com>
2023-09-25 11:21:23 +0530

    Signin page

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import Onboarding from './src/screens/auth/Onboarding';
-
-
+import { NavigationContainer } from '@react-navigation/native';
+import 'react-native-gesture-handler';
 
 const App = () => {
   return(
-    <Onboarding />
+    <NavigationContainer>
+
+      <Onboarding />
+    </NavigationContainer>
   )
 };
 
